c5a300e99d9b8a670d3389019e121f99d20b2d62
Esme Kwon <esme.kwon@yahoo.com>
2022-02-20 14:51:54 +0900

    else if 이용

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -46,16 +46,24 @@ class MainActivity : AppCompatActivity() {
 
             btnConditionPractice.setOnClickListener {
 
-                val userAge = 15
+                val userAge = 10
 
                 if (userAge >= 20) {
 
 //                    userAge의 값이 20 이상이면 실행됨.
                     Toast.makeText(this,"성인입니다.", Toast.LENGTH_SHORT).show()
                 }
+                else if (userAge >=17){
+
+                    Toast.makeText(this, "고등학생입니다.", Toast.LENGTH_SHORT).show()
+                }
+
+                else if(userAge >= 14) {
+                    Toast.makeText(this,"중학생입니다", Toast.LENGTH_SHORT).show()
+                }
                 else{
 
-                    Toast.makeText(this,"미성년자입니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this,"초등학생 혹은 그 이하입니다..", Toast.LENGTH_SHORT).show()
 
                 }
             }
